548a00ea3a5b155392ed975d0f10f6cccef1cde4
jorgitodf <jspaiva.1977@gmail.com>
2018-07-17 17:05:10 -0300

    Primeiro Componente

---
+++ front/src/components/Projects.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <h1 class="red">{{ mensagem }}</h1>
+        <p>Projeto Prático da SON</p>
+    </div>
+</template>
+
+<style>
+  .red {
+    background-color: red;
+    color: white;
+  }
+</style>
+
+<script>
+  export default {
+    data() {
+      return {
+        mensagem: 'Olá Mundo'
+      }
+    },
+    mounted() {
+      console.log('Estamos Prontos para Trabalhar');
+      //document.querySelector('h1.red').innerHTML = 'Olá Mundo';
+    }
+  }
+</script>
+
+
+
+
+
